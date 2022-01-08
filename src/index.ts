e356c61b40e4e5d82b952fb0b798200cc2c45353
mkabumattar <mohammad.khaled@outlook.com>
2022-01-08 19:59:44 +0200

    dev post

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import 'module-alias/register'
 import validateEnv from '@/utils/validate.env'
 import App from './app'
 import PostController from '@/controllers/post.controller'
+import UserController from '@/controllers/user.controller'
 
 validateEnv()
 
-const app = new App([new PostController()], Number(process.env.PORT))
+const app = new App(
+  [new PostController(), new UserController()],
+  Number(process.env.PORT)
+)
+
 app.listen()
